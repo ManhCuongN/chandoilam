a6289ec4f82c3be425008f59e695a10d85d91763
CuongKonBug <nmcuong.cpr@gmail.com>
2022-01-22 16:10:24 +0700

    kj

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
-MONGODB_URI =
-  "mongodb+srv://admin:admin@cluster0.nxcyn.mongodb.net/VideoCall?retryWrites=true&w=majority";
 async function connect() {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://admin:admin@cluster0.nxcyn.mongodb.net/VideoCall?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      }
+    );
     console.log("Connect successfully!!");
   } catch (error) {
-    console.log("Connect failure!!");
+    console.log(error);
   }
 }
 module.exports = { connect };
